a13cf45e9cb03e6c563bfbca2353a8708a866da5
Shiyi <shiyiya.11@gmail.com>
2019-09-03 13:26:43 +0800

    preview.

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 import map from './i.json'
 
-document.querySelectorAll('img').forEach(img => {
-  var k = parseInt(Math.random() * (map.length + 1), 10)
-  img.src = 'https://raw.githubusercontent.com/shiyiya/QI-ABSL/master/i/' + map[k]
+map.forEach(() => {
+  var imgEl = document.createElement('img')
+  var k = parseInt(Math.random() * map.length, 10)
+  console.log(k < map.length && k > -1)
+  imgEl.src =
+    'https://raw.githubusercontent.com/shiyiya/QI-ABSL/master/i/' + map[k]
+  document.body.append(imgEl)
+})
+
+
+map.forEach(() => {
+  var imgEl = document.createElement('img')
+  var k = parseInt(Math.random() * map.length, 10)
+  console.log(k < map.length && k > -1)
+  imgEl.src =
+    'https://raw.githubusercontent.com/shiyiya/QI-ABSL/master/c/' + map[k]
+  document.body.append(imgEl)
 })
